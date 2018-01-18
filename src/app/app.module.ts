49223c50c9c37cdf2a61faa5f49119cc54d0781e
younghyun-shin <yh.shin@dream-ant.com>
2018-01-18 20:13:26 +0900

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { PeopleServiceProvider } from '../providers/people-service/people-service';
+
+import {HttpModule} from '@angular/http';
 
 @NgModule({
   declarations: [
@@ -14,6 +17,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,7 +28,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    PeopleServiceProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PeopleServiceProvider } from '../../providers/people-service/people-service';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,25 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  netResponse:any;
+  constructor(public navCtrl: NavController, public network: PeopleServiceProvider) {
 
   }
+  callGetService(name,city)
+  {
+    let p = this.network.callGet(name,city);
+    p.then(data => {
+      console.log("Received " + JSON.stringify(data.json().args));
+      this.netResponse = JSON.stringify(data.json().args);
+    })
+  }
 
+  callPostService(name,city)
+  {
+    let p = this.network.callPost(name,city);
+    p.then(data => {
+      console.log("Received " + JSON.stringify(data.json().data));
+      this.netResponse = data.json().data;
+    })
+  }
 }
